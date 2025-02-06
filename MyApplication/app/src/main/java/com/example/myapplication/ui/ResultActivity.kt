9185fbbb78fabb00d5091f1e4a43324355a61381
jocelynsa23 <jsotelo20155@gmail.com>
2025-02-06 08:43:58 -0500

    Pagina de inicio de sesión implementada correctamente

--- MyApplication/app/src/main/java/com/example/myapplication/ui/ResultActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/ResultActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,14 +10,26 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.myapplication.R
 
 class ResultActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge() // Esta función activa el modo Edge-to-Edge
         setContentView(R.layout.activity_result)
+
+        // Configuración de las ventanas y márgenes
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Establecer OnClickListener para el botón de inicio
+        val buttonInicio = findViewById<Button>(R.id.button_inicio)
+        buttonInicio.setOnClickListener {
+            // Crear un Intent para iniciar LoginActivity
+            val intent = Intent(this@ResultActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish() // Opcional: Cierra ResultActivity si ya no la necesitas
         }
     }
 }
