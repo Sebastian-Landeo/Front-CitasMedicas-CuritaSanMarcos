b09d1b828f951b8566d5fe9da3603bf3d25bc86d
jocelynsa23 <jsotelo20155@gmail.com>
2025-02-06 16:13:05 -0500

    Redirección boton registro

--- MyApplication/app/src/main/java/com/example/myapplication/ui/LoginActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/LoginActivity.kt
@@ -1,17 +1,30 @@
 package com.example.myapplication.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapplication.R
 
+
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
+        // Configurar el OnClickListener para el botón de registro
+        val buttonRegistro=findViewById<Button>(R.id.buttonRegistro)
+        buttonRegistro.setOnClickListener {
+            // Crear un Intent para ir a RegisterActivity
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Configurar el manejo de los insets (barras del sistema)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
