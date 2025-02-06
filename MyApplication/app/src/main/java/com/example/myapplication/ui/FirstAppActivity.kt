31a2235269192ec2e564cc3266e0ad19b8ceac57
jocelynsa23 <jsotelo20155@gmail.com>
2025-02-06 01:08:43 -0500

    Correción pagina de inicio

---
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/FirstAppActivity.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.R
+
+class FirstAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_first_app)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Después de 5 segundos, ir a la actividad ResultActivity
+            val intent = Intent(this, ResultActivity::class.java)
+            startActivity(intent)
+            finish()  // Finaliza la actividad actual para evitar que el usuario regrese a ella
+        }, 4000)  // 5000 milisegundos = 5 segundos
+    }
+}
